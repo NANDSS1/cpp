55de14d4fb4958f80752b0cb8471a64a60096c7a
NANDSS1 <1175172084@qq.com>
2023-12-31 11:01:56 +0800

    vector<int> dp(n+1,INT_MAX);,dp[j] = min(dp[j],dp[j-i*i]+1)

---
+++ dp/lc279_wanquanpingfangshu.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int numSquares(int n) {
+        //完全背包问题，一件物品可以取很多次
+        //排列还是组合问题，不明确，先当组合问题来用，先遍历物品
+        
+
+    }
+};
