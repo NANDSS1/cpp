32870dbb17cbd55adfad9676b667c6c8c3ca5e1d
NANDSS1 <1175172084@qq.com>
2024-01-02 09:19:33 +0800

    算法

---
+++ dp/lc53_zuidazishuzuhe.cpp
@@ -0,0 +1,41 @@
+/*给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+子数组 是数组中的一个连续部分。
+
+ 
+
+示例 1：
+
+输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+输出：6
+解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+示例 2：
+
+输入：nums = [1]
+输出：1
+示例 3：
+
+输入：nums = [5,4,-1,7,8]
+输出：23
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp(nums.size(),0);
+        for(int i = 0;i < nums.size();i++) dp[i] = nums[i];
+        for(int i = 1;i < nums.size();i++){
+            dp[i] = max(dp[i],dp[i-1]+nums[i]);
+        }
+
+        int ans = INT_MIN;
+        for(const auto& num:dp){
+            ans = max(ans,num);
+        }
+
+        return ans;
+
+    }
+};
